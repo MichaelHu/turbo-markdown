dcb6c067ba67bc0cc4712bc706e7dcef286ce42e
hudamin <hdm258i@gmail.com>
2019-05-18 01:31:01 +0800

    v2: update bin/tm and markdown.bat; add windows platform support

--- markdown.bat
+++ markdown.bat
@@ -2,17 +2,19 @@
 setlocal enabledelayedexpansion
 
 set TMPFILE=%1.__tmp__
-set PREVIEWFILE=%1.preview.html
-set MARKDOWNCMD=C:\markdown_preview\win\markdown.exe
-set ROOT=C:\markdown_preview
+set PREVIEWFILE=%1.html
+set ROOT=%MARKDOWN_TURBO_ROOT%
+set MARKDOWNCMD=%MARKDOWN_TURBO_CMD%
 set TPL=tpl
+set ISPREVIEW=1
 
 if "%2" == "local" set TPL=tpl_local
 
-type %1 "%ROOT%\file_empty_line" > "%TMPFILE%"
+type "%1" "%ROOT%\file_empty_line" > "%TMPFILE%"
 
 %MARKDOWNCMD% "%TMPFILE%" > "%ROOT%\tmp\__tmp__.md.html" 
 type "%ROOT%\!TPL!\header.tpl.html" "%ROOT%\tmp\__tmp__.md.html" "%ROOT%\!TPL!\footer.tpl.html" > "%PREVIEWFILE%" 
+
 rem must no double-quotes
 start %PREVIEWFILE%
 
